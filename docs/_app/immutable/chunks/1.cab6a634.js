5de88dc128d6088b6e64b007912aa45b01168419
hazycora <hazycora@riseup.net>
2023-03-01 17:52:51 -0600

    build for gh pages

---
+++ docs/_app/immutable/chunks/1.cab6a634.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.f7cf20b2.js";export{t as component};
--- docs/_app/immutable/chunks/2-976c392f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-9c49d4c1.js";export{t as component};
